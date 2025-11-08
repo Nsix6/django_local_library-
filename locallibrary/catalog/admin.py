2b68478d0b10d4f5e6aee2e753807caafc7bd76e
Nsix6 <nsikakmbre@gmail.com>
2025-11-08 01:02:48 +0100

    addes base and index template

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -4,8 +4,39 @@ from django.contrib import admin
 
 from .models import Author, Genre, Book, BookInstance, Language
 
-admin.site.register(Book)
-admin.site.register(Author)
+# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+
+# Register the Admin classes for Book using the decorator
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+
 admin.site.register(Genre)
-admin.site.register(BookInstance)
 admin.site.register(Language)
+
+
+# not that the admin.site.register() does same as the @admin.register() decorator
+# Using the decorator, because it allows for customization and to show how
